3d78fd541666992be448b5fde68d3766f2004824
남서윤 <namseoyun231@gmail.com>
2025-11-04 00:04:23 +0900

    Todoitem수정

---
+++ src/components/Todoitem.tsx
@@ -0,0 +1,49 @@
+import type { Todo } from "../App";
+
+type TodoItemProps = {
+    todo: Todo;
+    onToggle: (id: number) => void;
+    onRemove: (id: number) => void;
+};
+
+export default function TodoItem({ todo, onToggle, onRemove }: TodoItemProps) {
+    return (
+        <li
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "8px 0",
+                borderBottom: "1px solid #dee2e6",
+            }}
+        >
+            <label style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+                <input
+                    type="checkbox"
+                    checked={todo.done}
+                    onChange={() => onToggle(todo.id)}
+                />
+                <span
+                    style={{
+                        textDecoration: todo.done ? "line-through" : "none",
+                        color: todo.done ? "#868e96" : "#212529",
+                    }}
+                >
+          {todo.text}
+        </span>
+            </label>
+            <button
+                onClick={() => onRemove(todo.id)}
+                style={{
+                    background: "transparent",
+                    border: "none",
+                    color: "#ff6b6b",
+                    fontSize: "16px",
+                    cursor: "pointer",
+                }}
+            >
+                ✕
+            </button>
+        </li>
+    );
+}
