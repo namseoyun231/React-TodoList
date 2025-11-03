8a9b3845f58e5cc62402136dd69afd37bbc49c78
남서윤 <namseoyun231@gmail.com>
2025-11-03 23:45:25 +0900

    Todoinsert수정

---
+++ src/components/Todoinsert.tsx
@@ -0,0 +1,31 @@
+import { useState, type FormEvent, type ChangeEvent } from "react";
+
+
+export default function TodoInsert({onAdd,}: {
+    onAdd: (text: string) => void;
+}) {
+    const [value, setValue] = useState("");
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // 페이지 새로고침 방지
+        const text = value.trim();
+        if (!text) return;
+        onAdd(text);
+        setValue("");
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
+            <button type="submit">추가</button>
+        </form>
+    );
+}
