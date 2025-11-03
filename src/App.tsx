5c3f1ab5b74625b36ef483ca0d4bb4c3305408b0
남서윤 <namseoyun231@gmail.com>
2025-11-04 00:16:07 +0900

    App.tsx수정

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState, useRef } from "react";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/Todoinsert";
+import TodoList from "./components/TodoList";
+
+export type Todo = {
+    id: number;
+    text: string;
+    done: boolean;
+};
+
+export default function App() {
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const nextId = useRef(1);
+
+    const handleAdd = (text: string) => {
+        setTodos((prev) => [
+            ...prev,
+            { id: nextId.current++, text, done: false },
+        ]);
+    };
+
+    const handleToggle = (id: number) => {
+        setTodos((prev) =>
+            prev.map((todo) =>
+                todo.id === id ? { ...todo, done: !todo.done } : todo
+            )
+        );
+    };
+
+    const handleRemove = (id: number) => {
+        setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    };
+
+    return (
+        <TodoTemplate>
+            <h1 style={{ textAlign: "center" }}>Todo List</h1>
+            <TodoInsert onAdd={handleAdd} />
+            <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
+        </TodoTemplate>
+    );
+}
