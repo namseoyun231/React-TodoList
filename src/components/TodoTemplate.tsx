87c8a2c69edb7570e4c0759207f907ebf9347b80
남서윤 <namseoyun231@gmail.com>
2025-11-03 23:19:45 +0900

    TodoTemplate수정

---
+++ src/components/TodoTemplate.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TodoTemplate({
+    children,}:
+                                     {
+    children: React.ReactNode;
+}){
+// 스타일은 최소화: 기본 레이아웃만 div로 감쌈
+    return <div>{children}</div>;
+}
