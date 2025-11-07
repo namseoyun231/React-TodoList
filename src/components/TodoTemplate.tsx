2eb2d1115c850540ec2b5618985ee81947621c57
남서윤 <namseoyun231@gmail.com>
2025-11-07 21:34:44 +0900

    TodoTemplate.tsx수정

--- src/components/TodoTemplate.tsx
+++ src/components/TodoTemplate.tsx
@@ -6,5 +6,20 @@ export default function TodoTemplate({
                                          children: React.ReactNode;
                                      }){
 // 스타일은 최소화: 기본 레이아웃만 div로 감쌈
-    return <div>{children}</div>;
+    return (<div
+            style={{
+                maxWidth: "420px",
+                margin: "0 auto",
+                paddingTop: "40px",
+            }}
+        >
+            <h1 style={{
+                fontSize: "36px",
+                marginBottom: "20px",
+                fontWeight: 700,
+                color: "#223",
+            }}>
+                Todo List
+            </h1>{children}</div>
+    );
 }
